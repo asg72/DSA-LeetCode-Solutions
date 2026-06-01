fe1ab4d0ef074c71eea0dbb8e0a950a36cbcf921
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-06-01 23:59:28 +0530

    Refactor minimum cost calculation for candies

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int minimumCost(vector<int>& cost) {
-        sort(cost.begin(),cost.end());
-
-        int v = 0;
-        long long m = 0;
-        for(int i=cost.size()-1; i>=0; i--){
-            if(v==2){
-                v = 0;
-            } else{
-                m += cost[i];  
-                v++;
+        // 1. Sort in ascending order so the most expensive candies are at the end
+        sort(cost.begin(), cost.end());
+        
+        int totalCost = 0;
+        
+        // 2. Buy candies in groups of 3, starting from the most expensive
+        for (int i = cost.size() - 1; i >= 0; i -= 3) {
+            // Always pay for the 1st most expensive candy in the triplet
+            totalCost += cost[i];
+            
+            // If a 2nd candy exists in this triplet, pay for it too
+            if (i - 1 >= 0) {
+                totalCost += cost[i - 1];
             }
+            
+            // The 3rd candy (at index i - 2) is skipped entirely because it's free!
         }
-        return m;
+        
+        return totalCost;
     }
 };
