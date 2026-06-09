d496bff73cce3db9301c82bf9edb75c0595fd79e
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-06-09 17:51:45 +0530

    Optimize pivotArray function for partitioning

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& arr, int p) {
         int n = arr.size();
-        vector<int> ans;
-        ans.reserve(n);
-
-        int c=0;
-
-        for(int i=0; i<n; i++){
-            if(arr[i]<p){
-                ans.push_back(arr[i]);
-            } else if(arr[i] == p){
-                c++;
+        vector<int> ans(n, p); // Pre-fill the array with the pivot value
+        
+        int left = 0;
+        int right = n - 1;
+        
+        // Single pass from both ends
+        for (int i = 0, j = n - 1; i < n; ++i, --j) {
+            if (arr[i] < p) {
+                ans[left++] = arr[i];
+            }
+            if (arr[j] > p) {
+                ans[right--] = arr[j];
             }
         }
-        for(int i=0; i<c; i++){
-            ans.push_back(p);
-        }
-
-        for(int i=0; i<n; i++){
-            if(arr[i]>p){
-                ans.push_back(arr[i]);
-            }
-        }
+        
         return ans;
     }
 };
