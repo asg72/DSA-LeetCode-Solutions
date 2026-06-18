e10ad530ff7f27b563fdd1593e32fed62d85eb69
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-06-18 13:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (46.25%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    /**
+     * Calculates the smaller angle (in degrees) formed between the hour and minute hand.
+     * Time Complexity: O(1)
+     * Space Complexity: O(1)
+     */
     double angleClock(int hour, int minutes) {
-        double ans = abs((30*hour)-(5.5*minutes));
-
-        if(ans>180){
-            ans = 360-ans;
+        // Hour hand moves 30 degrees per hour, plus 0.5 degrees per minute.
+        // Minute hand moves 6 degrees per minute.
+        // Formula simplified: abs(30 * hour + 0.5 * minutes - 6 * minutes)
+        double angle = abs((30.0 * hour) - (5.5 * minutes));
+        
+        // If the angle is reflex (greater than 180), take the smaller side
+        if (angle > 180.0) {
+            angle = 360.0 - angle;
         }
-
-        return ans;
+        
+        return angle;
     }
 };
