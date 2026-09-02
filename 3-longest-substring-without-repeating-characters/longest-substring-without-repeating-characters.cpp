4c5495155b41573c8bc86f7d433d0822486081ac
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-02 17:14:23 +0530

    Time: 3 ms (90.20%) | Memory: 18.6 MB (54.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        unordered_map<char, int> seen;
+        
+        // A simple array tracking frequencies of all 256 possible ASCII characters
+        int seen[256] = {0}; 
 
-        int low = 0;
-        int high = 0;
-        int maxLen = 0;
+        int low = 0, high = 0, maxLen = 0;
 
         while (high < n) {
             seen[s[high]]++;
 
             while (seen[s[high]] > 1) {
-                seen[s[low]]--;
-                if (seen[s[low]] == 0) {
-                    seen.erase(s[low]);
-                }
-                    low++;
+                seen[s[low]]--; // No need to erase! A count of 0 is perfectly fine here.
+                low++;
             }
 
             maxLen = max(maxLen, high - low + 1);
