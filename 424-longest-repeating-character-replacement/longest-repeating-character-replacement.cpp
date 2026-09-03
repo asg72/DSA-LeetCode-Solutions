4ab228cd59a4c7999d63047087c02358afae2f39
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-03 17:25:03 +0530

    Time: 98 ms (5.19%) | Memory: 11 MB (41.86%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -18,7 +18,6 @@ public:
                 low++;
 
                 len = high - low + 1;
-                // maxFreq = *std::ranges::max_element(f);
                 diff = len - maxFreq;
             }
 
