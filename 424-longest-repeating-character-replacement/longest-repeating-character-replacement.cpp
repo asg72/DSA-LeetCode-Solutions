499d6feb3a28d64ca9245abfe822d8594596090b
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-03 17:14:18 +0530

    Time: 159 ms (5.19%) | Memory: 10.9 MB (71.54%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int f[256] = {0};
+
+        int low = 0, high = 0, maxLen = 0;
+
+        while (high < n) {
+            f[s[high]]++;
+
+            int len = high - low + 1;
+            int maxFreq = *std::ranges::max_element(f);
+            int diff = len - maxFreq;
+
+            while (diff > k) {
+                f[s[low]]--;
+                low++;
+
+                len = high - low + 1;
+                maxFreq = *std::ranges::max_element(f);
+                diff = len - maxFreq;
+            }
+
+            maxLen = max(maxLen, high - low + 1);
+            high++;
+        }
+
+        return maxLen;
+    }
+};
