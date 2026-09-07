21efe6b0ff2b567de0252c1b0082217f238d16f8
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-07 22:49:11 +0530

    Time: 181 ms (5.10%) | Memory: 11.9 MB (7.73%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool isTrue(vector<int> &have , vector<int> &need){
-        for(int i=0; i<256; i++){
-            if(have[i] < need[i]){
+    bool isTrue(vector<int>& have, vector<int>& need) {
+        for (int i = 0; i < 256; i++) {
+            if (have[i] < need[i]) {
                 return false;
             }
-        }    
+        }
         return true;
     }
 
@@ -13,23 +13,24 @@ public:
         int n = s.size();
         int m = t.size();
 
-        vector<int>need(256, 0);
-        vector<int>have(256, 0);
-        
-        for(int i=0; i<m; i++){
+        vector<int> need(256, 0);
+        vector<int> have(256, 0);
+
+        for (int i = 0; i < m; i++) {
             need[t[i]]++;
         }
-        
-        if(n < m) return "";
+
+        if (n < m)
+            return "";
 
         int high = 0, low = 0, res = INT_MAX, start = -1;
 
-        while(high < n){
+        while (high < n) {
             have[s[high]]++;
-            
-            while(isTrue(have, need)){
+
+            while (isTrue(have, need)) {
                 int len = high - low + 1;
-                if(res > len){
+                if (res > len) {
                     res = len;
                     start = low;
                 }
@@ -40,7 +41,8 @@ public:
             high++;
         }
 
-        if(res == INT_MAX)  return "";
+        if (res == INT_MAX)
+            return "";
 
         return s.substr(start, res);
     }
