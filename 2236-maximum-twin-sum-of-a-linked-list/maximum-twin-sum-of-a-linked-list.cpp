e5ead6fbe52a43b6c575ac8ee1523a9c8321d938
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-18 18:41:47 +0530

    Time: 1 ms (74.31%) | Memory: 124.3 MB (65.53%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -13,36 +13,33 @@ public:
     int pairSum(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        
-        // 1. Move fast by 2 steps and slow by 1 step to find the middle
-        while (fast != nullptr && fast->next != nullptr) {
+
+        while(fast != nullptr && fast->next != nullptr){
             slow = slow->next;
             fast = fast->next->next;
         }
-        
-        // At this point, 'slow' points to the start of the second half
-        // 2. Reverse the second half of the linked list
-        ListNode* prev = nullptr;
+
         ListNode* curr = slow;
-        while (curr != nullptr) {
-            ListNode* nextNode = curr->next;
+        ListNode* prev = nullptr;
+
+        while(curr != nullptr){
+            ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = nextNode;
+            curr = next;
         }
-        
-        // 'prev' now points to the head of the reversed second half
-        ListNode* firstHalf = head;
-        ListNode* secondHalf = prev;
-        int maxTwinSum = 0;
-        
-        // 3. Iterate through both halves simultaneously to find the max twin sum
-        while (secondHalf != nullptr) {
-            maxTwinSum = max(maxTwinSum, firstHalf->val + secondHalf->val);
-            firstHalf = firstHalf->next;
-            secondHalf = secondHalf->next;
+
+        int maxSum = 0;
+        while(prev != nullptr){
+            int value = head->val + prev->val;
+            if(value > maxSum){
+                maxSum = value;
+            }
+
+            head = head->next;
+            prev = prev->next;
         }
-        
-        return maxTwinSum;
+
+        return maxSum;
     }
 };
