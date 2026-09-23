2bdafa65ad3f3eebf6019f4c73f79d5f9726a3c9
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-23 20:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (89.57%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    int maxSum(vector<int>& arr){
-        int bestEnding = arr[0];
-        int res = arr[0];
+    int maxAbsoluteSum(vector<int>& arr) {
+        int minEnding = arr[0];
+        int maxEnding = arr[0];
+        int minRes = arr[0];
+        int maxRes = arr[0];
+        
 
-        for(int i=1; i<arr.size(); i++){
+        for (int i = 1; i < arr.size(); i++) {
             int v1 = arr[i];
-            int v2 = arr[i] + bestEnding;
+            int v2Max = arr[i] + maxEnding;
+            int v2Min = arr[i] + minEnding;
 
-            bestEnding = max(v1, v2);
-            res = max(res, bestEnding);
+            minEnding = min(v1, v2Min);
+            maxEnding = max(v1, v2Max);
+
+            minRes = min(minRes, minEnding);
+            maxRes = max(maxRes, maxEnding);
         }
 
-        return res;
-    }
-
-    int minSum(vector<int>& arr){
-        int bestEnding = arr[0];
-        int res = arr[0];
-
-        for(int i=1; i<arr.size(); i++){
-            int v1 = arr[i];
-            int v2 = arr[i] + bestEnding;
-
-            bestEnding = min(v1, v2);
-            res = min(res, bestEnding);
-        }
-
-        return res;
-    }
-
-    int maxAbsoluteSum(vector<int>& nums) {
-        int max_Sum = maxSum(nums);
-        int min_Sum = minSum(nums);
-
-        int maxAbsSum = max(max_Sum, abs(min_Sum));
+        int maxAbsSum = max(maxRes, abs(minRes));
 
         return maxAbsSum;
     }
