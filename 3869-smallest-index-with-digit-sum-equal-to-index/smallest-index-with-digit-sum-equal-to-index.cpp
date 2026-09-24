c7cb9df410f9896ee1473dbc1aa55c659fa8260c
Aamratansh Singh Gahlot <187178492+asg72@users.noreply.github.com>
2026-09-24 20:08:37 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (81.66%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
-
         for (int i = 0; i < nums.size(); i++) {
             int val = nums[i];
             int sum = 0;
@@ -10,12 +9,11 @@ public:
                 sum += val % 10;
                 val = val / 10;
             }
-
+            
             if (i == sum) {
                 return i;
             }
         }
-
         return -1;
     }
 };
